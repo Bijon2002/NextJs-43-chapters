20883adeea26464ce382e9e4a3569e3711f9f605
Bijon2002 <bijosilin02@gmail.com>
2026-05-15 13:26:05 +0530

    feat: create About page and simplify Home page layout

--- blogs-today/app/about/page.tsx
+++ blogs-today/app/about/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Bijon</h1>
+      <h1>about page Bijon</h1>
     </main>
   );
 }
